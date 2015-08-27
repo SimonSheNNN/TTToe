f56737bbf4aeaae683f40007a2c67e0d2b3a59b5
SimonSheNNN <shensmsimon@gilmour.org>
2015-08-27 14:30:56 -0400

    8/26

--- src/ttt.java
+++ src/ttt.java
@@ -1,11 +1,66 @@
 
 
 public class ttt {
-	
 	public static void main (String [] args)
 	{private String[][] board;
      private static int ROWS = 3;
      private static int COLUMNS = 3;
+     
+     public ttt()
+     {
+          board = new String[ROWS][COLUMNS];
+          initializeBoard();
+     }
+     
+     
+     /** initialize board*/
+     public void initializeBoard ()
+     {
+          for (int i = 0; i < ROWS; i++)
+               for (int j = 0; j < COLUMNS; j++)
+               board [i][j] = " ";
+     
+	}
+     
+     
+     //check if occupied
+     //if (!board [(i)][j)].equals (" "))
+     //keep going
+     
+     
+     
+     //System.out.println("Please enter your desired X coordinate");
+     //System.out.println("Please enter your desired Y coordinate");
+     
+     
+     /** check winner*/
+     
+     public String checkBoard ()
+     {
+    	  for (int i = 0; i < ROWS; i ++)
+              if (board [i][0].equals(board [i][1]) &&
+                  board[i][1].equals(board[i][2]) &&
+                  !board[i][0].equals (" "))
+            	  // need more 
+            	  
+      	  for (int j = 0; j < COLUMNS; j ++)
+              if (board [0][j].equals(board [1][j]) &&
+                  board[1][j].equals(board[2][j]) &&
+                  !board[0][j].equals (" "))
+              	 // same here
+            	  
+          if (board [0][0].equals(board [1][1]) &&
+                  board[1][1].equals(board[2][2]) &&    
+                  !board[0][0].equals (" "))
+        	  
+          if (board [0][2].equals(board [1][1]) &&
+                  board[1][1].equals(board[2][0]) && 
+                  !board[0][2].equals (" "))
+        	  
+        	  
+        	  System.out.println("You won!");
+     }
+     // end initializeBoard method
      
      //define string instances for player1's mark x, player2's mark o, and the board;
 	//initializing the board to a 3x3 array;
